9bd900cdcda316f58ea0800174645e28efd1f8a6
七華 幸地 <nanakanjs@gmail.com>
2024-10-09 15:12:41 +0900

    問題文の末尾を自然な文章に訂正

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -23,13 +23,13 @@ int main()
 	//掛け算
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは？";
 	questions[0].a = x * y;
 
 	//割り算
 	x = uniform_int_distribution<>(1, 100)(rand);
 	y = uniform_int_distribution<>(1, 10)(rand);
-	questions[1].q = to_string(x * y) + "/" + to_string(y);
+	questions[1].q = to_string(x * y) + "/" + to_string(y) + "の答えは？";
 	questions[1].a = x;
 
 	//複雑な式
@@ -37,7 +37,7 @@ int main()
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 30)(rand);
 	int w = uniform_int_distribution<>(1, 20)(rand);
-	questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")/" + to_string(w);
+	questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")/" + to_string(w) + "の答えは？";
 	questions[2].a = x - (y + z);
 
 
@@ -52,7 +52,7 @@ int main()
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions) {
-		cout << e.q << "の答えは？\n";
+		cout << e.q << "\n";
 		int answer;
 		cin >> answer;
 		if (answer == e.a) {
