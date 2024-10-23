df05c09ab2690e461a5184a3065cc972b43147af
七華 幸地 <nanakanjs@gmail.com>
2024-10-23 15:29:51 +0900

    問題の配列をシャッフルしました

--- recruit_quiz/utility.h
+++ recruit_quiz/utility.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <vector>
 
-//ランダムな番号配列を作成する
+//配列をシャッフルする
+void shuffle(std::vector<int>& indices);
+
+//シャッフルした番号配列を作成する
 std::vector<int> CreateRandomIndices(int n);
+
+//間違った番号の配列を作成する
+// n
+// correctIndex
+std::vector<int>CreateWrongIndices(int n, int correctIndex);
