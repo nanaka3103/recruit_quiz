b00f9c30c81448200e2c59a1f54d9acb734693a6
七華 幸地 <nanakanjs@gmail.com>
2024-12-18 16:59:40 +0900

    地理の問題が出題されるようにプログラムを追加しました

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -2,6 +2,7 @@
 #include "exam_japanese.h"
 #include "exam_english.h"
 #include "exam_science.h"
+#include "exam_geography.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -102,7 +103,7 @@ int main()
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=理科\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=物理\n5=地理\n";
 	int subject;
 	cin >> subject;
 	if (subject == 2) {
@@ -119,12 +120,12 @@ int main()
 		questions = CreateEnglishWordExam();
 		QuestionList phraseExam = CreateEnglishPhraseExam();
 		questions.insert(questions.end(), phraseExam.begin(), phraseExam.end());
-	}
-	else if (subject == 4) {
+	} else if (subject == 4) {
 		questions = CreatePhysicsExam();
+	} else if (subject == 5) {
+		questions = CreatePrefecturesExam();
 	}
-
-
+	
 	for (const auto& e : questions) {
 		cout << e.q << "\n";
 		string answer;
