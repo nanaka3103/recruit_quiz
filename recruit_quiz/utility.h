a23d5eaa4d89e67ce21b537f293006af60d5114b
七華 幸地 <nanakanjs@gmail.com>
2024-12-18 17:01:15 +0900

    地理の問題を追加しました

--- recruit_quiz/utility.h
+++ recruit_quiz/utility.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <string>
 
 //配列をシャッフルする
 void shuffle(std::vector<int>& indices);
@@ -11,3 +12,9 @@ std::vector<int> CreateRandomIndices(int n);
 // n
 // correctIndex
 std::vector<int>CreateWrongIndices(int n, int correctIndex);
+
+// 文字列を分割する
+// s 分解する文字列
+// c 区切り文字
+std::vector<std::string> Split(const std::string& s, char c);
+
